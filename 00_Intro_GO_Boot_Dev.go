0c4010506f08d9d8992f43eb87ae3ac12db01e62
cespirandelli <espirandelli@gmail.com>
2023-05-16 15:56:40 -0300

    Update 00_Intro_GO_Boot_Dev.go

---
+++ 00_Intro_GO_Boot_Dev.go
@@ -0,0 +1,10 @@
+// GO is much faster than JS, Python, Ruby and PHP.
+// This happens because GO is a compiled language
+// Every interpreted programming language is slower, by it's nature.
+//---------------------//
+
+// Welcome to "Learn GO" from Boot.Dev
+// For the next lessons, we'll write a "Textio" Web Server
+
+// Since this program builds into an executable, we start using
+package main
